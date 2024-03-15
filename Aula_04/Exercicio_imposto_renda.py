6b7574e431d5bcd43ba09ff60bba95c6d980076a
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2024-03-15 18:49:29 -0300

    Add files via upload

--- Aula_04/Exercicio_imposto_renda.py
+++ Aula_04/Exercicio_imposto_renda.py
@@ -3,25 +3,29 @@ salarioBruto = input("Digite seu salário atual: ")
 salarioBruto = float(salarioBruto)
 
 if salarioBruto <= 2112:
-    print("Apenas Salário bruto de R$%.2f"%(salarioBruto))
+    aliquota = 0
+    deducao = 0
+    print("Salário bruto de R$%.2f"%(salarioBruto))
+    
 elif salarioBruto > 2112 and salarioBruto <= 2826.65:
-    aliquota = salarioBruto*0.075
-    deducao = aliquota - 158.40
-    salarioLiquido = salarioBruto - deducao
+    aliquota = 0.075
+    deducao = 150.40 
 
 elif salarioBruto > 2826.66 and salarioBruto <= 3751.05:
-    aliquota = salarioBruto*0.15
-    deducao = aliquota - 370.40 
-    salarioLiquido = salarioBruto - deducao
+    aliquota = 0.15
+    deducao = 370.40 
 
 elif salarioBruto > 3751.06 and salarioBruto <= 4664.68:
-    aliquota = salarioBruto*0.225
-    deducao = aliquota - 651.73 
-    salarioLiquido = salarioBruto - deducao
+    aliquota = 0.225
+    deducao = 651.73 
 
 elif salarioBruto > 4664.68:
-    aliquota = salarioBruto*0.275
-    deducao = aliquota - 884.96 
+    aliquota = 0.275
+    deducao = 884.96 
+
+if(aliquota != 0 and deducao != 0):
+    aliquota = salarioBruto*aliquota
+    deducao = aliquota - deducao
     salarioLiquido = salarioBruto - deducao
 
-print("Salario bruto: %f  IRRF: %f ")
+    print("Salario bruto: R$%.2f  Alicota: R$%.2f  Dedução: R$%.2f  Salário liquido: R$%.2f" %(salarioBruto,aliquota,deducao,salarioLiquido))
