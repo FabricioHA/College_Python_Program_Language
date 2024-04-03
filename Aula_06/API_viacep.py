3893d1ca9482ac74a1dec23d20fea1440f6e7bd2
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2024-04-03 20:32:37 -0300

    Delete Aula_06/API_viacep.py

--- Aula_06/API_viacep.py
+++
@@ -1,6 +0,0 @@
-# https://viacep.com.br/ws/09664000/json/
-
-import requests #Permite ao python requisitar recursos externos na internet, por exemplo
-import warnings #Bibliotecas referentes a mensagens de erros
-warnings.filterwarnings("ignore")
-
